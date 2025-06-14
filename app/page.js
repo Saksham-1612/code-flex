c668f01b5d8e83de4d5edef1e71a5b7dd37aaab4
saksham.srivastava <saksham.srivastava@kapturecrm.com>
2025-06-15 01:36:48 +0530

    added logo

--- app/page.js
+++ app/page.js
@@ -5,12 +5,19 @@ import TestimonialsSection from "@/components/Main/Testimonial-Section";
 import Instructors from "@/components/Main/Instructors";
 import PlacementStats from "@/components/Main/PlacementStats";
 import OurPrograms from "@/components/Main/OurPrograms";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Wrapper>
       <div className="flex flex-col items-center">
-        <TextHoverEffect text="CODEFLEX" />
+        {/* <TextHoverEffect text="CODEFLEX" /> */}
+        <Image
+          src="/CodeflexLogo.png"
+          alt="codeflex"
+          width={100}
+          height={100}
+        />
         <HeroSection />
         <Instructors />
         <PlacementStats />
